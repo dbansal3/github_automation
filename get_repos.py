f595c93d5032488e4a527d6c6aba0bc2d8e8cffb
dbansal3 <102465555+dbansal3@users.noreply.github.com>
2022-09-06 12:33:22 +0530

    Update get_repos.py

--- get_repos.py
+++ get_repos.py
@@ -2,7 +2,7 @@ from github import Github
 
 # using an access token
 g = Github('dbansal3', 'Deepanshu@2022')
-print ("res : ".format(g))
+print ("res : {}".format(g))
 
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
