cd671aecfdbf7ac485b6c015dcb41e65b049460a
dbansal3 <102465555+dbansal3@users.noreply.github.com>
2022-09-06 12:04:22 +0530

    Update get_collaborator.py

--- get_collaborator.py
+++ get_collaborator.py
@@ -1,1 +1,18 @@
-get
+def get_project_collaborators_by_username(project, include_permissions=True):
+    """Returns a JSON representation of the collaborators in the given project"""
+    collaborators = {}
+
+    for collaborator in project.can_view_group.user_set.all():
+        collaborators[collaborator.username] = _get_collaborator_json(
+            collaborator, include_permissions, can_edit=False
+        )
+
+    for collaborator in itertools.chain(project.owners_group.user_set.all(), project.can_edit_group.user_set.all()):
+        collaborators[collaborator.username] = _get_collaborator_json(
+            collaborator, include_permissions, can_edit=True
+        )
+
+    return collaborators 
+  
+  
+get_project_collaborators_by_username('sonar-scanning-examples')
