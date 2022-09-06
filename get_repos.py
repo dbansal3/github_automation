0a57d0e8e9344f9d42a5a2cf683fb658766bceda
dbansal3 <102465555+dbansal3@users.noreply.github.com>
2022-09-06 12:37:02 +0530

    Update get_repos.py

--- get_repos.py
+++ get_repos.py
@@ -7,6 +7,11 @@ print ("res : {}".format(g))
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
 
+user = g.get_user()
+print(user.name)
+print(user.login)
+
+
 for repo in g.get_user().get_repos():
     print(repo.name)
     repo.edit(has_wiki=False)
