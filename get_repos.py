5bac39f914f99edea3fcd6859e7670e1de217ab1
dbansal3 <102465555+dbansal3@users.noreply.github.com>
2022-09-06 12:30:51 +0530

    Create get_repos.py

---
+++ get_repos.py
@@ -0,0 +1,8 @@
+from github import Github
+
+# using an access token
+g = Github('dbansal3', 'Deepanshu@2022')
+
+# Github Enterprise with custom hostname
+# g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+
